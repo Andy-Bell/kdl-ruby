6a98cb5f7d33446baa9cda65cec2cc4c9f543ba9
Andy Bell <andy.bell.github@gmail.com>
2020-12-26 13:55:03 +0000

    bundle init gem

---
+++ lib/kdl/ruby.rb
@@ -0,0 +1,8 @@
+require "kdl/ruby/version"
+
+module Kdl
+  module Ruby
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/kdl/ruby/version.rb
@@ -0,0 +1,5 @@
+module Kdl
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end
