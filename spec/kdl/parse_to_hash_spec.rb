727e6f23111c68c936c3d5b651fd4d0bdbdf110f
Andy Bell <andy.bell.github@gmail.com>
2020-12-28 14:20:09 +0000

    Add basic.kdl file to get initial parser working

---
+++ spec/kdl/parse_to_hash_spec.rb
@@ -0,0 +1,35 @@
+RSpec.describe Kdl::Main do
+  it "has a version number" do
+    expect(Kdl::Main::VERSION).not_to be nil
+  end
+
+  it 'parses the string' do
+    file = File.read("spec/fixtures/files/basic.kdl")
+    hash = Kdl::Main::Parse.to_h(file)
+
+    expect(hash).to eq({"foo" => "foo", "bar" => "bar"})
+  end
+
+  it 'parses the cargo.kdl file' do
+    file = File.read("spec/fixtures/files/cargo.kdl")
+
+    hash = Kdl::Main::Parse.to_h(file)
+
+    expect(hash).to eq(
+      {
+        "package" => {
+          "name" => "kdl",
+          "version" => "0.0.0",
+          "description" => "kat's document language",
+          "authors" => "Kat Marchán <kzm@zkat.tech>",
+          "license-file" => "LICENSE.md",
+          "edition" => "2018",
+        },
+        "dependencies" => {
+          "nom" => "6.0.1",
+          "thiserror" => "1.0.22",
+        },
+      }
+    )
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require "bundler/setup"
-require "kdl/ruby"
+require "kdl/main"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
